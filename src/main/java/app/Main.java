47ac57f608f495111c9912931e3a6495328e6517
KIYOUNGYI <yky2798@gmail.com>
2021-05-17 13:27:17 +0900

    dev-005

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -14,20 +14,20 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class Main {
 
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
-//    System.exit(SpringApplication.exit(SpringApplication.run(Main.class, args)));//async 용 (잘 종료가 안되는 경우가 있어서 추가)
+//    SpringApplication.run(Main.class, args);
+    System.exit(SpringApplication.exit(SpringApplication.run(Main.class, args)));//async 용 (잘 종료가 안되는 경우가 있어서 추가)
   }
 
   // spring 에서 taskExecutor 를 자동으로 등록해서 사용해서 프라이머리 지정 필요
-//  @Bean
-//  @Primary
-//  TaskExecutor taskExecutor() {
-//    ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-//    taskExecutor.setCorePoolSize(10);//코어 풀 사이즈
-//    taskExecutor.setMaxPoolSize(20);//최대 스레드의 사이즈
-//    taskExecutor.setThreadNamePrefix("batch-thread-");
-//    taskExecutor.initialize();
-//    return taskExecutor;
-//  }
+  @Bean
+  @Primary
+  TaskExecutor taskExecutor() {
+    ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+    taskExecutor.setCorePoolSize(10);//코어 풀 사이즈
+    taskExecutor.setMaxPoolSize(20);//최대 스레드의 사이즈
+    taskExecutor.setThreadNamePrefix("batch-thread-");
+    taskExecutor.initialize();
+    return taskExecutor;
+  }
 
 }
